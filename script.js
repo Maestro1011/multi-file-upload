e7ea0b49bdf7156bd68c71ad031cf7b5bce1b0b5
Maestro1011 <m2makamaestro1011@gmail.com>
2021-04-30 16:18:22 +0530

    Made changes for Styling, api key

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 let dropArea = document.getElementById("drop-area")
 
 // Prevent default drag behaviors
+
+// there is a semi colon which there to make sure the for each loop doesn't index over the windows on which this event listener will run
+
 ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
   dropArea.addEventListener(eventName, preventDefaults, false)   
   document.body.addEventListener(eventName, preventDefaults, false)
 })
 
 // Highlight drop area when item is dragged over it
+
+// there is a semi colon which there to make sure the for each loop doesn't index over the windows on which this event listener will run
+
 ;['dragenter', 'dragover'].forEach(eventName => {
   dropArea.addEventListener(eventName, highlight, false)
 })
+
+// there is a semi colon which there to make sure the for each loop doesn't index over the windows on which this event listener will run
 
 ;['dragleave', 'drop'].forEach(eventName => {
   dropArea.addEventListener(eventName, unhighlight, false)
@@ -21,6 +29,7 @@ dropArea.addEventListener('drop', handleDrop, false)
 function preventDefaults (e) {
   e.preventDefault()
   e.stopPropagation()
+  // propa prevents from same method being called again and again.
 }
 
 function highlight(e) {
@@ -38,6 +47,7 @@ function handleDrop(e) {
   handleFiles(files)
 }
 
+
 let uploadProgress = []
 let progressBar = document.getElementById('progress-bar')
 
@@ -48,17 +58,19 @@ function initializeProgress(numFiles) {
   for(let i = numFiles; i > 0; i--) {
     uploadProgress.push(0)
   }
+  console.log(uploadProgress)
 }
 
 function updateProgress(fileNumber, percent) {
   uploadProgress[fileNumber] = percent
   let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
-  console.debug('update', fileNumber, percent, total)
   progressBar.value = total
 }
 
 function handleFiles(files) {
+  console.log(files);
   files = [...files]
+  console.log(files);
   initializeProgress(files.length)
   files.forEach(uploadFile)
   files.forEach(previewFile)
@@ -67,6 +79,7 @@ function handleFiles(files) {
 function previewFile(file) {
   let reader = new FileReader()
   reader.readAsDataURL(file)
+  console.log(reader);
   reader.onloadend = function() {
     let img = document.createElement('img')
     img.src = reader.result
@@ -75,16 +88,21 @@ function previewFile(file) {
 }
 
 function uploadFile(file, i) {
-  var url = 'https://api.cloudinary.com/v1_1/dp7lg6rty/image/upload'
+  var url = 'https://api.cloudinary.com/v1_1/demo/image/upload'
   var xhr = new XMLHttpRequest()
+  // its an object that interacts with the server
+  // and saves the responce received
   var formData = new FormData()
   xhr.open('POST', url, true)
+  // makes a post setRequestHeader
   xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest')
+  // this sets the value of http request
 
   // Update progress (can be used to show progress indicator)
   xhr.upload.addEventListener("progress", function(e) {
     updateProgress(i, (e.loaded * 100.0 / e.total) || 100)
   })
+  // it is used to monitor the upload progess of the file
 
   xhr.addEventListener('readystatechange', function(e) {
     if (xhr.readyState == 4 && xhr.status == 200) {
@@ -95,7 +113,8 @@ function uploadFile(file, i) {
     }
   })
 
-  formData.append('upload_preset', 'dp7lg6rty')
+  formData.append('upload_preset', 'doc_codepen_example')
   formData.append('file', file)
   xhr.send(formData)
 }
+
